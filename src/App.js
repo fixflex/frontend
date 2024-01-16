fdc4e1a2a5c8ab4bb457d193018250b89dbcfb3d
“devMenna” <dev.mennaalaa@gmail.com>
2024-01-16 15:44:14 +0200

    ADD: creating navbar style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from './components/navbar/Navbar';
+
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <h3>Hello World!</h3>
     </div>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Grid, Box, Avatar, List, ListItem, Button } from '@mui/material';
+import styles from './navbar.module.css';
+import logo from '../../images/logo.jpg';
+
+const Navbar = () => {
+  return (
+    <Box className={styles.navContainer}>
+      <Grid container alignItems='center'>
+        <Grid item>
+          <Avatar
+            src={logo}
+            variant='rounded'
+            sx={{ width: 260, height: 70 }} // Adjust the width and height as needed
+          />
+        </Grid>
+        <Grid item>
+          <List className={styles.navList}>
+            <ListItem>
+              <Button className={styles.navButton}>Lorem</Button>
+            </ListItem>
+            <ListItem>
+              <Button className={styles.navButton}>Dolor</Button>
+            </ListItem>
+            <ListItem>
+              <Button className={styles.navButton}>Lorem</Button>
+            </ListItem>
+            <ListItem>
+              <Button className={styles.navButton}>Ipsum</Button>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+      <Grid item className={styles.rightNav}>
+        <Button variant='contained' className={styles.rightNavButtons}>
+          Join
+        </Button>
+        <Button variant='contained' className={styles.rightNavButtons}>
+          Login
+        </Button>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Navbar;
