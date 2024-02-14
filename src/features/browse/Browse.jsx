778a3bb05384b2548f00ef5050bcfe382e6de894
devMenna <dev.mennaalaa@gmail.com>
2024-02-14 16:54:39 +0200

    REFACTOR: design for tasks list

--- src/features/browse/Browse.jsx
+++ src/features/browse/Browse.jsx
@@ -3,7 +3,7 @@ import SecondaryNav from '../../components/secondary-nav/SecondaryNav';
 import TaskList from '../../components/task-list/TaskList';
 import TaskDetail from '../../components/task-detail/TaskDetail';
 import { Grid } from '@mui/material';
-import styles from './browse.module.css'; // Your CSS module file for Browse
+import styles from './browse.module.css';
 
 const Browse = ({ tasks }) => {
   return (
@@ -14,18 +14,14 @@ const Browse = ({ tasks }) => {
           <Grid item xs={12} md={4}>
             <div className={styles.taskContainer}>
               {' '}
-              {/* Add this line */}
               <TaskList tasks={tasks} />
             </div>{' '}
-            {/* Add this line */}
           </Grid>
           <Grid item xs={12} md={8}>
             <div className={styles.taskContainer}>
               {' '}
-              {/* Add this line */}
               <TaskDetail />
             </div>{' '}
-            {/* Add this line */}
           </Grid>
         </Grid>
       </div>
