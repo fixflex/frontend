34d8bc2bcdc135a72ba98868a77f4610c2075818
devMenna <dev.mennaalaa@gmail.com>
2024-04-24 03:32:41 +0200

    ADD: advanced filtering of tasks

--- src/components/secondary-nav/SecondaryNav.jsx
+++ src/components/secondary-nav/SecondaryNav.jsx
@@ -1,97 +1,203 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
-  InputBase,
+  TextField,
+  InputAdornment,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
   Button,
-  Modal,
-  Box,
-  FormControlLabel,
-  Checkbox,
 } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { alpha } from '@mui/material/styles';
 import styles from './secondaryNav.module.css';
+import { useSelector } from 'react-redux';
 
-const SecondaryNav = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  const [modalOpen, setModalOpen] = useState(false);
+const SecondaryNav = ({
+  setTitleFilter,
+  setCategoryFilter,
+  setLocationFilter,
+  setSortOption,
+}) => {
+  const categories = useSelector((state) => state.categories.categoriesList);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  // Dummy category data and state for the example
-  const [categories, setCategories] = useState([
-    { id: 1, name: 'Category 1', checked: false },
-    { id: 2, name: 'Category 2', checked: false },
-    // Add more categories as needed
-  ]);
-
-  // Handle checkbox change
-  const handleCheckboxChange = (id) => {
-    setCategories(
-      categories.map((category) =>
-        category.id === id
-          ? { ...category, checked: !category.checked }
-          : category
-      )
-    );
+  const handleFilterInput = (e) => {
+    setTitleFilter(e.target.value);
   };
 
-  const modalStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 300,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
+  const handleCategoryInput = (e) => {
+    setCategoryFilter(e.target.value);
   };
 
-  const handleOpen = () => setModalOpen(true);
-  const handleClose = () => setModalOpen(false);
+  const handleLocationInput = (e) => {
+    setLocationFilter(e.target.value);
+  };
 
-  const renderFilters = () =>
-    categories.map((category) => (
-      <FormControlLabel
-        key={category.id}
-        control={
-          <Checkbox
-            checked={category.checked}
-            onChange={() => handleCheckboxChange(category.id)}
-          />
-        }
-        label={category.name}
-      />
-    ));
+  const handleSortOptions = (e) => {
+    setSortOption(e.target.value);
+  };
 
   return (
-    <div className={styles.secondaryNav}>
-      <InputBase
-        placeholder='Search for a task'
-        className={styles.searchInput}
+    <div className={styles.container}>
+      <TextField
+        variant='outlined'
+        onChange={handleFilterInput}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position='start'>
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          sx: {
+            borderRadius: '50px',
+            '& fieldset': {
+              borderColor: alpha('#ccc', 0.5),
+            },
+            '&:hover fieldset': {
+              borderColor: '#161C1E',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#1976d2',
+            },
+            backgroundColor: 'white',
+          },
+        }}
+        sx={{
+          width: '200px',
+          '& .MuiInputBase-input': {
+            color: '#6c757d',
+          },
+        }}
       />
-      {!isMobile && <div className={styles.filters}>{renderFilters()}</div>}
-      <Button onClick={handleOpen} className={styles.filterButton}>
-        {isMobile ? 'Filter' : 'Advanced Search'}
-      </Button>
-      <Modal
-        open={modalOpen}
-        onClose={handleClose}
-        aria-labelledby='filter-modal-title'
-        aria-describedby='filter-modal-description'
+      <FormControl className={styles.dropdown}>
+        <InputLabel
+          sx={{
+            color: '#212121',
+            fontWeight: 'bold',
+            '&.Mui-focused': {
+              color: '#212121',
+            },
+          }}
+        >
+          Categories
+        </InputLabel>
+        <Select
+          defaultValue=''
+          sx={{
+            borderRadius: '50px',
+            '& fieldset': {
+              borderColor: alpha('#ccc', 0),
+            },
+            '&:hover fieldset': {
+              borderColor: '#3fa1ce40',
+              backgroundColor: '#3fa1ce40',
+              color: 'white',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#3fa1ce40',
+            },
+            backgroundColor: 'transparent',
+            color: '#212121 !important',
+          }}
+          onChange={handleCategoryInput}
+        >
+          {categories.map((category) => (
+            <MenuItem key={category.id} value={category.id}>
+              {category.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl className={styles.dropdown}>
+        <InputLabel
+          sx={{
+            color: '#212121',
+            fontWeight: 'bold',
+            '&.Mui-focused': {
+              color: '#212121',
+            },
+          }}
+        >
+          Location
+        </InputLabel>
+        <Select
+          defaultValue=''
+          sx={{
+            borderRadius: '50px',
+            '& fieldset': {
+              borderColor: alpha('#ccc', 0),
+            },
+            '&:hover fieldset': {
+              borderColor: '#3fa1ce40',
+              backgroundColor: '#3fa1ce40',
+              color: 'white',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#3fa1ce40',
+            },
+            backgroundColor: 'transparent',
+            color: '#212121 !important',
+          }}
+          onChange={handleLocationInput}
+        >
+          <MenuItem value='online'>Online</MenuItem>
+          <MenuItem value='inPerson'>In Person</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={styles.dropdown}>
+        <InputLabel
+          sx={{
+            color: '#212121',
+            fontWeight: 'bold',
+            '&.Mui-focused': {
+              color: '#212121',
+            },
+          }}
+        >
+          Sort
+        </InputLabel>
+        <Select
+          defaultValue=''
+          sx={{
+            borderRadius: '50px',
+            '& fieldset': {
+              borderColor: alpha('#ccc', 0),
+            },
+            '&:hover fieldset': {
+              borderColor: '#3fa1ce40',
+              backgroundColor: '#3fa1ce40',
+              color: 'white',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: '#3fa1ce40',
+            },
+            backgroundColor: 'transparent',
+            color: '#212121 !important',
+          }}
+          onChange={handleSortOptions}
+        >
+          <MenuItem value='priceLowHigh'>Price Low to High</MenuItem>
+          <MenuItem value='priceHighLow'>Price High to Low</MenuItem>
+          <MenuItem value='newestOldest'>Newest to Oldest</MenuItem>
+          <MenuItem value='oldestNewest'>Oldest to Newest</MenuItem>
+        </Select>
+      </FormControl>
+      <Button
+        variant='contained'
+        sx={{
+          color: 'white',
+          borderRadius: '10px',
+          backgroundColor: '#212121',
+        }}
+        onClick={() => {
+          setCategoryFilter('');
+          setTitleFilter('');
+          setLocationFilter('');
+          setSortOption('');
+        }}
       >
-        <Box sx={modalStyle}>
-          <h2 id='filter-modal-title'>Filters</h2>
-          <div id='filter-modal-description'>{renderFilters()}</div>
-          <Button onClick={handleClose} className={styles.applyButton}>
-            Apply Filters
-          </Button>
-        </Box>
-      </Modal>
+        Reset Filters
+      </Button>
     </div>
   );
 };
--- src/components/task-list/TaskList.jsx
+++ src/components/task-list/TaskList.jsx
@@ -9,10 +9,61 @@ import {
   Person2,
   Public,
 } from '@mui/icons-material';
+import dayjs from 'dayjs';
 
-const TaskList = ({ setIsModalOpen, isMobile }) => {
+const TaskList = ({
+  setIsModalOpen,
+  isMobile,
+  titleFilter,
+  categoryFilter,
+  locationFilter,
+  sortOption,
+}) => {
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.allTasks.tasks);
+  const formatDate = (dueDate) => {
+    return dueDate.flexible ? 'Flexible' : dueDate.on;
+  };
+
+  // Filtering logic
+  const filteredTasks = tasks.filter((task) => {
+    const matchesTitle = titleFilter
+      ? task.title &&
+        task.title.toLowerCase().includes(titleFilter.toLowerCase())
+      : true;
+
+    const matchesCategory = categoryFilter
+      ? task.categoryId && task.categoryId === categoryFilter
+      : true;
+
+    const matchesLocation = locationFilter
+      ? locationFilter === 'online'
+        ? task.location?.online
+        : !task.location?.online
+      : true;
+
+    return matchesTitle && matchesCategory && matchesLocation;
+  });
+
+  // Sorting logic
+  const sortedTasks = [...filteredTasks].sort((a, b) => {
+    if (!sortOption) {
+      return 0;
+    }
+
+    switch (sortOption) {
+      case 'priceLowHigh':
+        return a.budget - b.budget;
+      case 'priceHighLow':
+        return b.budget - a.budget;
+      case 'newestOldest':
+        return dayjs(b.createdAt).unix() - dayjs(a.createdAt).unix();
+      case 'oldestNewest':
+        return dayjs(a.createdAt).unix() - dayjs(b.createdAt).unix();
+      default:
+        return 0;
+    }
+  });
 
   const handleSelectTask = (task) => {
     dispatch(selectTask(task));
@@ -21,13 +72,9 @@ const TaskList = ({ setIsModalOpen, isMobile }) => {
     }
   };
 
-  const formatDate = (dueDate) => {
-    return dueDate.flexible ? 'Flexible' : dueDate.on;
-  };
-
   return (
     <div className={styles.taskListContainer}>
-      {tasks.map((task, index) => (
+      {sortedTasks.map((task) => (
         <Card
           key={task._id}
           className={styles.taskCard}
@@ -51,7 +98,6 @@ const TaskList = ({ setIsModalOpen, isMobile }) => {
                 {`${task.userId.firstName} ${task.userId.lastName}`}
               </Typography>
             </Box>
-
             <Box
               sx={{ display: 'flex', alignItems: 'center', margin: '1rem 0' }}
               mt={2}
@@ -61,7 +107,6 @@ const TaskList = ({ setIsModalOpen, isMobile }) => {
                 {formatDate(task.dueDate)}
               </Typography>
             </Box>
-
             {task.location.online ? (
               <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <Public />
